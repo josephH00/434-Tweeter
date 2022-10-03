c73623107964f45982dc11645365f2931881c632
josephH00 <12785608+josephH00@users.noreply.github.com>
2022-10-02 18:49:03 -0700

    Formatting: tracker.cpp

--- Tracker/src/tracker.cpp
+++ Tracker/src/tracker.cpp
@@ -3,18 +3,16 @@
 
 #include "Server/Server.h"
 
-
-
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    if( argc != 2 ) {
+    if (argc != 2)
+    {
         std::cout << "Usage: " << argv[0] << "[Server Port Number]" << std::endl;
         std::exit(1);
     }
 
     Server tracker(
-        std::atoi(argv[1])
-    );
+        std::atoi(argv[1]));
 
     tracker.run();
 }
