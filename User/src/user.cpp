8bb0024607e92a4766058c5c34f05a9f9697389d
josephH00 <12785608+josephH00@users.noreply.github.com>
2022-10-15 04:28:11 -0700

    Removed remnants of C Socket example code

--- User/src/user.cpp
+++ User/src/user.cpp
@@ -1,26 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
 
 #include "Common/Structures.h"
 #include "User/User.h"
 
-void DieWithError(const char *errorMessage) // External error handling function
-{
-    perror(errorMessage);
-    exit(1);
-}
-
 int main(int argc, char *argv[])
 {
-
     if (argc < 3) // Test for correct number of arguments
     {
-        fprintf(stderr, "Usage: %s <Server IP address> <Echo Port>\n", argv[0]);
-        exit(1);
+        std::cerr << "Usage: " << argv[0] << " <Server IP address> <Echo Port>" << std::endl;
+        std::exit(1);
     }
 
     User user(
-        argv[1],      // First arg: Server IP addr
-        atoi(argv[2]) // Second arg: Server port
+        argv[1],           // First arg: Server IP addr
+        std::atoi(argv[2]) // Second arg: Server port
     );
 
     user.run();
